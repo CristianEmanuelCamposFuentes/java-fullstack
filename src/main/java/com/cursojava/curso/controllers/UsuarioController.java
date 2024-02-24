99a04ec2b35a67bd16b1d0abe3726c8ad95bb1a6
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-02-23 23:44:03 -0300

    Modify return to List

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -3,11 +3,13 @@ package com.cursojava.curso.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @RequestMapping(value = "/prueba")
-    public String prueba(){
-        return "prueba";
+    public List<String> prueba(){
+        return List.of("Manzana", "Pera", "Naranja");
     }
 }
