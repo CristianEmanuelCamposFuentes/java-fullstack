89121aefea3a0595db6cb9c04132d3f193c5450d
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-02-23 23:58:00 -0300

    Create User class

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "/prueba")
-    public List<String> prueba(){
-        return List.of("Manzana", "Pera", "Naranja");
+    @RequestMapping(value = "/usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Cristian");
+        usuario.setApellido("Campos");
+        usuario.setEmail("cristiancampos@example.com");
+        usuario.setTelefono("123456789");
+        return usuario;
     }
 }
