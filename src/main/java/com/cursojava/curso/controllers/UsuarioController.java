7cd39d21b151c1d184c05c1f09c9e810f9f1ddbd
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-02-25 16:21:44 -0300

    Create Users CRUD methods

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,16 +1,44 @@
 package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "/usuario")
-    public Usuario getUsuario(){
+    @RequestMapping(value = "/usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Cristian");
+        usuario.setApellido("Campos");
+        usuario.setEmail("cristiancampos@example.com");
+        usuario.setTelefono("123456789");
+        return usuario;
+    }
+    @RequestMapping(value = "/usuario2")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Cristian");
+        usuario.setApellido("Campos");
+        usuario.setEmail("cristiancampos@example.com");
+        usuario.setTelefono("123456789");
+        return usuario;
+    }
+    @RequestMapping(value = "/usuario3")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Cristian");
+        usuario.setApellido("Campos");
+        usuario.setEmail("cristiancampos@example.com");
+        usuario.setTelefono("123456789");
+        return usuario;
+    }
+    @RequestMapping(value = "/usuario4")
+    public Usuario buscar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Cristian");
         usuario.setApellido("Campos");
